228ecd5e15c45aeece3f50854ebad7b21d8b73d7
Jason Swift <anemele@outlook.com>
2023-12-04 16:04:15 +0800

    add [send2trash](https://github.com/arsenetar/send2trash)

--- recycle/__main__.py
+++ recycle/__main__.py
@@ -3,17 +3,30 @@ import glob
 from itertools import chain
 
 from .log import logger
-from .recycle import recycle
 
-parser = argparse.ArgumentParser(__package__, description=__doc__)
-parser.add_argument(
-    'file', nargs='+', type=str, help='Files or folders sent to the recycle bin'
-)
 
-args = parser.parse_args()
-logger.debug(f'{args=}')
+def get_args():
+    parser = argparse.ArgumentParser(__package__, description=__doc__)
+    parser.add_argument(
+        'file', nargs='+', type=str, help='Files or folders sent to the recycle bin'
+    )
 
-file: list[str] = args.file
-for sth in chain.from_iterable(map(glob.iglob, file)):
-    logger.info(f'done: {sth}')
-    recycle(sth)
+    args = parser.parse_args()
+    logger.debug(f'{args=}')
+
+    file: list[str] = args.file
+    return file
+
+
+files = chain.from_iterable(map(glob.iglob, get_args()))
+
+
+# from .recycle import recycle
+
+# for sth in files:
+#     logger.info(f'done: {sth}')
+#     recycle(sth)
+
+from .send2trash import send2trash
+
+send2trash(files)
